b25091343d8216d26fe35cbe1a84d3b1284f4ba0
CHANDUBALAGANI <chandubalagani10@gmail.com>
2026-08-01 12:02:09 +0530

    Add dynamic category icons

--- products/models.py
+++ products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
         return self.name
