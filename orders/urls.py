c097edf96013eb4ac2bd87b802debf0074563cb7
CHANDUBALAGANI <chandubalagani10@gmail.com>
2026-08-06 15:45:38 +0530

    Complete order management module

--- orders/urls.py
+++ orders/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path("checkout/", views.checkout, name="checkout"),
     path("success/", views.order_success, name="order_success"),
     path("my-orders/", views.my_orders, name="my_orders"),
+
+    path(
+        "details/<int:order_id>/",
+        views.order_detail,
+        name="order_detail"
+    ),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import get_object_or_404
 from .forms import CheckoutForm
 from .models import Order, OrderItem
 from products.models import Cart, CartItem
@@ -100,7 +100,39 @@ def checkout(request):
 @login_required
 def my_orders(request):
 
-    return render(request, "orders/my_orders.html")
+    orders = Order.objects.filter(
+        user=request.user
+    ).order_by("-created_at")
+
+    return render(
+        request,
+        "orders/my_orders.html",
+        {
+            "orders": orders
+        }
+    )
+
+@login_required
+def order_detail(request, order_id):
+
+    order = get_object_or_404(
+        Order,
+        id=order_id,
+        user=request.user
+    )
+
+    items = OrderItem.objects.filter(
+        order=order
+    ).select_related("product")
+
+    return render(
+        request,
+        "orders/order_detail.html",
+        {
+            "order": order,
+            "items": items,
+        }
+    )
 
 
 @login_required
