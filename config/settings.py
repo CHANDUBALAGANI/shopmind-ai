246a9f3a5f24d00b5e40a9516326b4b3996a14aa
CHANDUBALAGANI <chandubalagani10@gmail.com>
2026-08-10 15:55:33 +0530

    Temporarily enable production error details

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ CLOUDINARY_STORAGE = {
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "True").lower() == "true"
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "shopmind-ai-ykfq.onrender.com",
